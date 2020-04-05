d2e7efee08947d72211a73d92817f0dbb43dac3d
Ktb <marouan_k@live.be>
2020-04-05 19:29:23 +0200

    Amélioration

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ $(document).ready(function(){
     $('#valideEmail').hide()
     $('#valideMessage').hide()
 
+    // $('.navbar a , footer a').on("click", function(event){
+    //     event.preventDefault();
+    //     var hash = this.hash;
+
+    //     $('body, html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+    // })
+
     $('#submitt').click(function(event){
         
 
